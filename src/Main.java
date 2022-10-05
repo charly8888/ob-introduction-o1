2a51735b714eb86cdeed9ada15a224e2f53f7632
German Hornus <hornusgerman@gmail.com>
2022-10-05 07:10:44 -0500

    create class Coche and function suma

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
 
     public static void main(String[] args){
-    int resultado = 0;
-    resultado = suma(4,5);
-    System.out.println(resultado);
-    }
-    public static int suma(int a, int b) {
 
-        return  a + b;
+     int resultado = suma(4,5, 111);
+    System.out.println(resultado);
+
+    var Toyota = new Coche();
+
+    Toyota.moreDoors();
+    System.out.println(Toyota.puertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return  a + b + c;
+    }
+
+    public static class Coche{
+        int puertas = 4;
+
+         void moreDoors(){
+            this.puertas ++;
+        }
     }
 }
